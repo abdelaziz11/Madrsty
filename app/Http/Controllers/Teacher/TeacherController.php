385c5f0d35d6bd8829079a1ecfe719fd819601a9
unknown <ahmedgamal1994.com>
2020-07-27 15:08:43 +0200

    middlewares

--- app/Http/Controllers/Teacher/TeacherController.php
+++ app/Http/Controllers/Teacher/TeacherController.php
@@ -2,77 +2,14 @@
 
 namespace App\Http\Controllers\Teacher;
 
-use Hash;
-use Session;
-use App\Model\Grade;
-use App\Model\Teacher;
-use App\Model\Subject;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 
 class TeacherController extends Controller
 {
-
     public function __construct()
     {
-        //some middlewares for user type permissions
-
-        $this->middleware('auth:teacher')->only('home');
-        $this->middleware('authAdmin')->only(['index', 'show']);
+        $this->middleware('auth:teacher');
     }
 
-    
-    public function home()
-    {
-        return view('teachers.home');
-    }
-
-
-    public function index()
-    {
-        //this function will show all the teachers
-        $teachers = Teacher::all();
-        return view('teachers.index', compact('teachers'));
-    }
-
-    public function show(Teacher $teacher)
-    {
-        //this function will show a specific teacher with his grades
-        $teacher_grades = $teacher->grades()->get();
-        return view('teachers.show', compact('teacher', 'teacher_grades'));
-    }
-
-    public function edit(Teacher $teacher)
-    {
-
-        //this function will show the teacher edit page
-        $grades = Grade::all();
-        $subjects = Subject::all();
-        return view('teachers.edit', compact('teacher', 'grades', 'subjects'));
-    }
-
-    public function update(Request $request, Teacher $teacher)
-    {
-
-        //this function will update a teacher
-        $teacher->update([
-            'name' => $request->name,
-            'email' => $request->email,
-            'password' => Hash::make($request->password),
-            'subject_id' => $request->subject_id,
-        ]);
-
-        $teacher->grades()->sync($request->grade_id);
-
-        Session::flash('success', 'Teacher Updated Successfully');
-        return redirect()->route('teachers.index');
-    }
-
-    public function destroy(Teacher $teacher)
-    {
-        //this function will delete a specific teacher
-        $teacher->delete();
-        Session::flash('success', 'Teacher Deleted Successfully');
-        return redirect()->route('teachers.index');
-    }
 }
--- routes/web.php
+++ routes/web.php
@@ -20,56 +20,19 @@ Auth::routes();
 Route::get('/home', 'HomeController@index')->name('home');
 
 Route::group(['prefix' => 'teacher'], function () {
-    Route::get('/login', 'User\UserController@login_form')->name('teachers.login-form');
-    Route::post('/login', 'User\UserController@login')->name('teachers.login');
     Route::get('/home', 'Teacher\TeacherController@home')->name('teachers.home');
 });
 
 Route::group(['prefix' => 'student'], function () {
-    Route::get('/login', 'Student\LoginController@login_form')->name('students.login-form');
-    Route::post('/login', 'Student\LoginController@login')->name('students.login');
+    Route::get('/home', 'Student\StudentController@home')->name('students.home');
+});
+
+Route::group(['middleware' => 'auth:teacher'], function () {
+    Route::get('/home', 'Student\StudentController@home')->name('students.home');
+});
+
+Route::group(['middleware' => 'auth:student'], function () {
     Route::get('/home', 'Student\StudentController@home')->name('students.home');
 });
 
 
-//this routes group will be available only for the admin
-Route::group(['prefix' => 'admin', 'middleware' => 'authAdmin'], function () {
-    
-    Route::resource('/grades', 'Grade\GradeController');
-    Route::group(['prefix' => 'grades'], function () {
-        Route::get('/{grade}/subjects', 'Grade\GradeController@grade_subjects')->name('grade.subjects');
-        Route::get('/{grade}/students', 'Grade\GradeController@grade_students')->name('grade.students');
-        Route::get('/{grade}/lectures', 'Grade\GradeController@grade_lectures')->name('grade.lectures');
-        Route::get('/{grade}/subjects/{subject}/lectures', 'Grade\GradeController@grade_subject_lectures')->name('grade.subject.lectures');
-        
-    });
-
-    Route::group(['prefix' => 'teachers'], function () {
-        Route::get('/', 'Teacher\TeacherController@index')->name('teachers.index');
-        Route::get('/register', 'Teacher\RegisterController@register_form')->name('teachers.register-form');
-        Route::post('/register', 'Teacher\RegisterController@register')->name('teachers.register');
-        Route::get('/{teacher}', 'Teacher\TeacherController@show')->name('teachers.show');
-        Route::get('/{teacher}/edit', 'Teacher\TeacherController@edit')->name('teachers.edit');
-        Route::put('/{teacher}/edit', 'Teacher\TeacherController@update')->name('teachers.update');
-        Route::delete('/{teacher}', 'Teacher\TeacherController@destroy')->name('teachers.destroy');
-    });
-
-    Route::group(['prefix' => 'students'], function() {
-        Route::get('/', 'Student\StudentController@index')->name('students.index');
-        Route::get('/register', 'Student\RegisterController@register_form')->name('students.register-form');
-        Route::post('/register', 'Student\RegisterController@register')->name('students.register');
-        Route::get('/{student}', 'Student\StudentController@show')->name('students.show');
-        Route::get('/{student}/edit', 'Student\StudentController@edit')->name('students.edit');
-        Route::put('/{student}/edit', 'Student\StudentController@update')->name('students.update');
-        Route::delete('/{student}', 'Student\StudentController@destroy')->name('students.destroy');
-    });
-
-    Route::resource('/subjects', 'Subject\SubjectController');
-    Route::resource('/lectures', 'Lecture\LectureController');
-
-});
-
-
-Auth::routes();
-
-Route::get('/home', 'HomeController@index')->name('home');
